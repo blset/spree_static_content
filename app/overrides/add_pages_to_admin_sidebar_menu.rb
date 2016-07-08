13491f8b548c05c0853d4240c72e88bd640e6551
blset <contact@blset.com>
2016-07-08 11:43:15 +0200

    deface sync

--- app/overrides/add_pages_to_admin_sidebar_menu.rb
+++ app/overrides/add_pages_to_admin_sidebar_menu.rb
@@ -1,7 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/layouts/admin',
   name: 'pages_admin_sidebar_menu',
-  original:  'a3bf24a1ad74eadb2098702cf008e5383787b9f8',
   insert_bottom: '#main-sidebar',
   partial: 'spree/admin/shared/pages_sidebar_menu'
 )
